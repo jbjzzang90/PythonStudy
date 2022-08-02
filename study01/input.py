4b8d2d6994afa2e00e292c87c06b32ad12ce73ed
bnjeong <bnjeong@cslac.com>
2022-08-02 14:43:55 +0900

    input 예제

---
+++ study01/input.py
@@ -0,0 +1,32 @@
+# 파이썬 사용자 입력
+# input 사용법
+# 기본 타입(str)
+
+# 예제 1
+# name = input("Enter Your Name")
+# grede = input("Enter Your Grede")
+# company = input("Enter Your Company Name")
+
+
+# 예제 2
+# number = input("Enter number : ")
+# name = input("Enter name : ")
+
+# print("type of number : ", type(number))
+# print("type of name : ", type(name))
+
+
+# 예제 3 (형변환)
+# frist_number = int(input("Enter number1 : "))
+# second_number = int(input("Enter number2 : "))
+# print('frist_number + second_number : ',frist_number + second_number)
+
+# 예제 4
+# float_number = float(input("Enter a float1 : "))
+# print('float : ',float_number)
+# print('type :  ',type(float_number))
+
+
+# 예제 5
+print("FirstName - {0}, LastName = {1}".format(input("Enter first Name : "),input("Enter second Name")))
+
